61316a4ac3fe15c86ea03a7c06a07bb59f2c297e
cedricdcc <cedricdecruw@gmail.com>
2025-08-19 14:01:59 +0200

    Add conventions document and update App.js to include conventions.md

---
+++ conventions.md
@@ -0,0 +1,28 @@
+# Marine Term Translation Repository Conventions
+
+This document outlines the conventions used for naming and organizing marine term translation repositories.
+
+## Repository Naming
+
+Each repository is named using the following format:
+
+```
+<VocabularyName>-<LanguageCode>
+```
+
+- **VocabularyName**: The short code or name for the vocabulary collection (e.g., `P02`).
+- **LanguageCode**: The two-letter ISO language code (e.g., `NL` for Dutch, `FR` for French).
+
+**Example:**
+- The Dutch translation for the P02 collection: `P02-NL`
+- The French translation for the P02 collection: `P02-FR`
+
+## Purpose
+
+This convention ensures clarity and consistency, making it easy to identify the vocabulary and language for each translation repository.
+
+## Additional Guidelines
+
+- Use uppercase letters for both vocabulary name and language code.
+- Avoid spaces or special characters in repository names.
+- Each repository should contain documentation specifying the vocabulary and language.
--- src/App.js
+++ src/App.js
@@ -1,61 +1,74 @@
-import { HashRouter, Route, Routes } from 'react-router-dom';
-import './App.css';
-import NavBar from './components/NavBar';
-import MarckdownViewer from './components/MarckdownViewer';
-import ListRepo from './components/ListRepo';
+import { HashRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import NavBar from "./components/NavBar";
+import MarckdownViewer from "./components/MarckdownViewer";
+import ListRepo from "./components/ListRepo";
 
 function App() {
-  
   return (
-    <HashRouter >
+    <HashRouter>
       <div className="App">
-        <NavBar/>
+        <NavBar />
         <Routes>
-          <Route path='/' element=
-          {
-            <MarckdownViewer fullLink="https://raw.githubusercontent.com/marine-term-translations/marine-term-translations.github.io/main/siteInfo.md"/>
-          } />
+          <Route
+            path="/"
+            element={
+              <>
+                <MarckdownViewer fullLink="https://raw.githubusercontent.com/marine-term-translations/marine-term-translations.github.io/main/siteInfo.md" />
+                <MarckdownViewer fullLink="https://raw.githubusercontent.com/marine-term-translations/marine-term-translations.github.io/main/conventions.md" />
+              </>
+            }
+          />
           {/* Navigation */}
 
           {/* Translater */}
-          <Route path='/list' element=
-          {
-            <ListRepo/>
-          } />
-          <Route path='/about' element=
-          {
-            <MarckdownViewer fullLink="https://raw.githubusercontent.com/marine-term-translations/Front-End/refs/heads/main/Information/Usage.md"/>
-          } />
+          <Route path="/list" element={<ListRepo />} />
+          <Route
+            path="/about"
+            element={
+              <MarckdownViewer fullLink="https://raw.githubusercontent.com/marine-term-translations/Front-End/refs/heads/main/Information/Usage.md" />
+            }
+          />
 
           {/* Developer */}
-          <Route path='/front' element=
-          {
-            //!\\
-            <MarckdownViewer fullLink=""/>
-          } />
-          <Route path='/back' element=
-          {
-            <MarckdownViewer fullLink="https://raw.githubusercontent.com/marine-term-translations/Back-End/main/Readme.md"/>
-          } />
-          <Route path='/gh_action' element=
-          {
-            <MarckdownViewer fullLink="https://raw.githubusercontent.com/vliz-be-opsci/ldes_translation_gh_action/main/README.md"/>
-          } />
-          <Route path='/general' element=
-          {
-            //!\\
-            <MarckdownViewer fullLink="https://raw.githubusercontent.com/marine-term-translations/marine-term-translations.github.io/main/README.md"/>
-          } />
+          <Route
+            path="/front"
+            element={
+              //!\\
+              <MarckdownViewer fullLink="" />
+            }
+          />
+          <Route
+            path="/back"
+            element={
+              <MarckdownViewer fullLink="https://raw.githubusercontent.com/marine-term-translations/Back-End/main/Readme.md" />
+            }
+          />
+          <Route
+            path="/gh_action"
+            element={
+              <MarckdownViewer fullLink="https://raw.githubusercontent.com/vliz-be-opsci/ldes_translation_gh_action/main/README.md" />
+            }
+          />
+          <Route
+            path="/general"
+            element={
+              //!\\
+              <MarckdownViewer fullLink="https://raw.githubusercontent.com/marine-term-translations/marine-term-translations.github.io/main/README.md" />
+            }
+          />
 
           {/* Administrator */}
-          <Route path='/instruction' element=
-          {
-            //!\\
-            <MarckdownViewer fullLink=""/>
-          } />
+          <Route
+            path="/instruction"
+            element={
+              //!\\
+              <MarckdownViewer fullLink="" />
+            }
+          />
         </Routes>
       </div>
-    </HashRouter >
+    </HashRouter>
   );
 }
 
